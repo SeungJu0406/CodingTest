453793867562a72633ea4ba8487074d7e9077831
SeungJu0406 <nss8164@gmail.com>
2024-08-05 15:49:57 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.18 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] result = new int[2];
+        for(int i = 0 ; i < num_list.Length;i++){
+            if(num_list[i]%2==0)
+                result[0]++;
+            else
+                result[1]++;
+        }
+        return result;
+    }
+}
