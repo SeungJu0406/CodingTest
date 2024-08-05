6a96feba4e5fd56cd39193e021dffbda1d6fc78d
SeungJu0406 <nss8164@gmail.com>
2024-08-05 20:08:05 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.84 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cs
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cs
@@ -3,14 +3,13 @@ using System.Linq;
 
 public class Solution {
     public int solution(int[] num_list) {
-        int answer;
-        if(num_list.Length<=10){
-            answer = 1;
-            foreach(int c in num_list){
-                answer*=c;            
-            }
-            return answer;            
+        return num_list.Length<=10?  Function(num_list) : num_list.Sum();
+    }
+    public int Function(int[] num_list){
+        int answer = 1;
+        foreach(int c in num_list){
+            answer*=c;            
         }
-        return num_list.Sum();
+        return answer;  
     }
 }
