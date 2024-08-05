87b1ea63bd78c51f2f932ca202356cf3f2642418
SeungJu0406 <nss8164@gmail.com>
2024-08-05 15:50:05 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.27 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int n) {
+        string result = "";
+        for(int i = 0 ; i < my_string.Length ; i++){
+            for(int j = 0 ; j < n ; j++){
+                result += my_string[i];
+            }
+        }
+        return result;
+    }
+}
