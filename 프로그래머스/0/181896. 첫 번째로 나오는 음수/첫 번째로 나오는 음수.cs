d39ca65bdb5334b229e173d9d90e5c156ca6d908
SeungJu0406 <nss8164@gmail.com>
2024-08-06 16:15:21 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.55 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
@@ -2,10 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int[] num_list) {
-        for(int i = 0 ; i<num_list.Length ; i++){
-            if(num_list[i]<0)
-                return i;
-        }
-        return -1;
+        return Array.FindIndex(num_list,f => f<0);
     }
 }
