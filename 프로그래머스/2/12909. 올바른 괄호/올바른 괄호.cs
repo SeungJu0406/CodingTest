8301aa70f73f26691e881ad83a609d8f7fff700b
SeungJu0406 <nss8164@gmail.com>
2024-08-07 21:59:22 +0900

    [level 2] Title: 올바른 괄호, Time: 1.72 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 public class Solution {
     public bool solution(string s) {
-        Stack<char> stack = new Stack<char>();
-        foreach(char c in s){
-            if(stack.Count>0&&( stack.Peek()=='('&& c==')' ))//스택에 아무것도 없을때, 마지막과 짝이 안맞을때
-                stack.Pop();
-            else
-                stack.Push(c);
-        }
-        return stack.Count==0? true : false;
+       Stack<char> stack = new Stack<char>();
+       foreach(char c in s){
+           if(stack.Count>0&&( stack.Peek()=='('&& c==')' ))//스택에 아무것도 없을때, 마지막과 짝이 안맞을때
+               stack.Pop();
+           else
+               stack.Push(c);
+       }
+       return stack.Count==0? true : false;
     }
 }
