b7c5c774d2acfba34cddb507e5a8fe48b8c16d3c
SeungJu0406 <nss8164@gmail.com>
2024-08-07 16:31:22 +0900

    [level 0] Title: 홀짝 구분하기, Time: 27.39 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        if(n%2==0)
+            Console.WriteLine($"{n} is even");
+        else
+            Console.WriteLine($"{n} is odd");
+    }
+}
