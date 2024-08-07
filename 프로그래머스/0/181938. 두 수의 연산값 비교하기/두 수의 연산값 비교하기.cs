9240d65bbb4dec5e9debb8c9244542255d60f03c
SeungJu0406 <nss8164@gmail.com>
2024-08-07 15:58:43 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.32 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b) {
+        int result1 = a*Rank(b) + b;
+        int result2 = 2*a*b;
+        return result1>=result2? result1 : result2;
+    }
+    public int Rank(int n){
+        if(n==0)
+            return 1;
+        return 10*Rank(n/10);
+    }
+}
