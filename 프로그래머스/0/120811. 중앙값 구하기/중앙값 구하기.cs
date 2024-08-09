c1725f97d07169b839fb8beb943dff68e7ac8621
SeungJu0406 <nss8164@gmail.com>
2024-08-09 11:46:03 +0900

    [level 0] Title: 중앙값 구하기, Time: 1.52 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
@@ -2,15 +2,8 @@ using System;
 
 public class Solution {
     public int solution(int[] array) {                
-        for(int i =0;i<array.Length;i++){
-            for(int j=0;j<array.Length;j++){
-                if(array[i]>=array[j]){
-                    int temp =array[i];
-                    array[i] = array[j];
-                    array[j] =temp;
-                }
-            }
-        }
-        return array[array.Length/2];
+        Array.Sort(array);
+        int midIndex=array.Length/2;
+        return array[midIndex];
     }
 }
