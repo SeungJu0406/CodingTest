d4ac5c3f3ccbe8beee24f238e89f0cb948e57f00
SeungJu0406 <nss8164@gmail.com>
2024-09-07 19:45:10 +0900

    [level 1] Title: 평균 구하기, Time: 0.77 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.cs
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cs
@@ -1,10 +1,7 @@
 using System.Linq;
 
 public class Solution {
-    public double solution(int[] arr) {
-        double answer = 0;
-        answer = (double)arr.Sum()/(double)arr.Length;
-        
-        return answer;
+    public double solution(int[] arr) {      
+        return arr.Average();
     }
 }
