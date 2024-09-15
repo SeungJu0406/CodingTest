71f728d523e482d58079adf92632fb1baf39356f
SeungJu0406 <nss8164@gmail.com>
2024-09-16 02:26:54 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.32 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -4,7 +4,7 @@ using System;
 public class Solution {
     public long solution(long n) {
         double sqrt = Math.Sqrt(n);
-        if(Math.Floor(sqrt) != sqrt)
+        if(sqrt%1 != 0)
             return -1;
         return (long)Math.Pow(sqrt+1,2);
         
