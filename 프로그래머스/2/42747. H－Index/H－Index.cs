19ee0f6bdd306d5d755781977faafd1959dfba8e
Keinex <nss8164@gmail.com>
2025-05-01 17:02:58 +0900

    [level 2] Title: H-Index, Time: 2.12 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public int solution(int[] citations)
+    {
+        int answer = 0;
+        
+        Array.Sort(citations);
+        Array.Reverse(citations);
+
+        for(int i = 0; i< citations.Length; i++)
+        {
+            if(citations[i] >= i+1 )
+            {
+                answer++ ;
+            }
+            else
+            {
+                break;
+            }
+        }
+      
+        return answer;
+    }
+}
